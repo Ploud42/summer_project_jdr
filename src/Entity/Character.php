53a206d387fcb8ad7abb5a29016ef59889bc9d0c
Julien Sobel <sobeljulien@gmail.com>
2022-07-18 11:37:43 +0200

    start api

--- src/Entity/Character.php
+++ src/Entity/Character.php
@@ -3,11 +3,13 @@
 namespace App\Entity;
 
 use App\Repository\CharacterRepository;
+use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CharacterRepository::class)]
+#[ApiResource]
 #[ORM\Table(name: '`character`')]
 class Character
 {
