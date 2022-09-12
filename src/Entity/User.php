47060bbfbb13d441235a337c485c6851464a4e33
Julien Sobel <sobeljulien@gmail.com>
2022-09-12 14:00:43 +0200

    want to add unique:true to pseudo : before

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,13 +6,17 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    normalizationContext: ['groups' => ['read']],
+    denormalizationContext: ['groups' => ['write']],
+)]
 #[ORM\Table(name: '`user`')]
 #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
@@ -20,21 +24,27 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
+    #[Groups(["read"])]
     private $id;
 
     #[ORM\Column(type: 'string', length: 180, unique: true)]
+    #[Groups(["read", "write"])]
     private $email;
 
     #[ORM\Column(type: 'json')]
+    #[Groups(["read"])]
     private $roles = [];
 
     #[ORM\Column(type: 'string')]
+    #[Groups("write")]
     private $password;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Groups(["read", "write"])]
     private $pseudo;
 
     #[ORM\OneToMany(mappedBy: 'user', targetEntity: Run::class)]
+    #[Groups(["read"])]
     private $runs;
 
     public function __construct()
